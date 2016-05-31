5912ee3ab6321f8db1a0bb7850ce7c7dd1e927db
JT1991 <joshthorn91@gmail.com>
2016-05-31 15:01:41 +0100

    Update media.py

--- python/Fav_Tv_Movie/media.py
+++ python/Fav_Tv_Movie/media.py
@@ -1,4 +1,4 @@
-#Creating Parent class Video
+# Creating Parent class Video
 class Video():
     def __init__(self, video_Title, video_Storyline, poster_Image, trailer_Youtube_Url):
         self.title = video_Title
@@ -6,13 +6,13 @@ class Video():
         self.trailer_youtube_url = trailer_Youtube_Url
         self.poster_image_url = poster_Image
 
-#Creating child class Movie, with parent class Video
+# Creating child class Movie, with parent class Video
 class Movie(Video):
     def __init__(self, video_Title, video_Storyline, poster_Image, trailer_Youtube_Url, movie_Rating):
         Video.__init__(self, video_Title, video_Storyline, poster_Image, trailer_Youtube_Url)
         self.rating = movie_Rating
 
-#Creating child class Tv_Show, with parent class Video
+# Creating child class Tv_Show, with parent class Video
 class Tv_Show(Video):
     def __init__(self, video_Title, video_Storyline,poster_Image, trailer_Youtube_Url, number_Seasons):
         Video.__init__(self, video_Title, video_Storyline, poster_Image, trailer_Youtube_Url)
