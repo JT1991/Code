ec2b943d6afe4b7699d820e5bcea52e8d134aa51
JT91 <joshthorn91@gmail.com>
2016-06-09 18:15:16 +0100

    Create aliens

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -12,13 +13,22 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     #make ship
     ship = Ship(ai_settings ,screen)
+    #Make a group to store bullets in
+    bullets = Group()
+    #Make an alien group
+    aliens = Group()
+
+    #Create fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     
     #Start main loop for game
     while True:
         #Watch for keyboard/mouse events
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
         #Redraw the screen during each pass through the loop
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
