b4e36effadd1b4dd961354959b5895d792579918
JT91 <joshthorn91@gmail.com>
2016-06-09 02:58:36 +0100

    Ship movement

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -11,12 +11,13 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     #make ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings ,screen)
     
     #Start main loop for game
     while True:
         #Watch for keyboard/mouse events
         gf.check_events(ship)
+        ship.update()
         #Redraw the screen during each pass through the loop
         gf.update_screen(ai_settings, screen, ship)
 
--- python/alien_invasion/ship.py
+++ python/alien_invasion/ship.py
@@ -1,8 +1,9 @@
 import pygame
 class Ship():
-    def __init__(self,screen):
+    def __init__(self, ai_settings, screen):
         """Intialize the ship and set its starting position."""
         self.screen = screen
+        self.ai_settings = ai_settings
         #Load ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -11,6 +12,23 @@ class Ship():
         #Start new ship at bottom centre
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        #Store decimal value for ships centre
+        self.center = float(self.rect.centerx)
+        
+        #Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update ships position based on movement flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
