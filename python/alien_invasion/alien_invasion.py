e66114c9d094e9d728e26726b16c07fe2d74e46f
JT91 <joshthorn91@gmail.com>
2016-06-10 16:19:15 +0100

    11

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -28,9 +28,12 @@ def run_game():
     while True:
         #Watch for keyboard/mouse events
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            
         #Redraw the screen during each pass through the loop
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
--- python/alien_invasion/game_stats.py
+++ python/alien_invasion/game_stats.py
@@ -5,9 +5,10 @@ class GameStats():
         """Initialise statistics"""
         self.ai_settings = ai_settings
         self.reset_stats()
+        # Start Alien Invasion in an active state.
+        self.game_active = True
 
     def reset_stats(self):
         """Init statistics that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
-        
         
